ad84fe877ac133a0f498559b18b9b9ff07146882
Pasha Altapov <2004pablo2004@gmail.com>
2023-12-20 13:59:01 +0300

    PR15.4

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 /* eslint-disable no-console */
+// eslint-disable-next-line import/no-unresolved
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
 const NotFound = require('./errors/NotFound');
@@ -20,6 +22,8 @@ const { login, postUser } = require('./controllers/users');
 const app = express();
 
 app.use(cookieParser());
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
